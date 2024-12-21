7e97dce870b5bb723a4e045c582d226ab6125e27
optimisticnpc <joshuajltan@gmail.com>
2024-12-22 07:56:09 +1300

    Finished 2b Forms

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,9 @@ import Note from './components/Note'
 const App = (props) => {
   const [notes, setNotes] = useState(props.notes)
   const [newNote, setNewNote] = useState(
-    'you are beautiful'
+    'a new note...'
   ) 
-
+  const [showAll, setShowAll] = useState(true)
   const addNote = (event) => {
     event.preventDefault()
     console.log('button clicked', event.target)
@@ -17,11 +17,20 @@ const App = (props) => {
     setNewNote(event.target.value)
   }
 
+  const notesToShow = showAll
+  ? notes
+  : notes.filter(note => note.important === true)
+
   return (
     <div>
       <h1>Notes</h1>
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? 'important' : 'all'}
+        </button>
+      </div>
       <ul>
-        {notes.map(note => 
+        {notesToShow.map(note =>
           <Note key={note.id} note={note} />
         )}
       </ul>
